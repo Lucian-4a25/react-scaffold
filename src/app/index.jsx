86e068b018f51877de19f7cd9313645ad42b436d
Lucian <lucian.journey@gmail.com>
2024-02-19 12:06:17 +0800

    support typescript

--- src/app/index.jsx
+++
@@ -1,5 +0,0 @@
-// Opt-in to Webpack hot module replacement
-// if (module.hot) module.hot.accept()
-import './index.css';
-
-export default () => "Hello, old react dddwada";
--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,24 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const webpack = require('webpack');
 
+const babelOpts = {
+  presets: [
+    ['@babel/preset-env', {
+      "targets": "defaults" 
+    }],
+    '@babel/preset-react'
+  ]
+};
+
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.[contenthash].js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
   },
   plugins: [
     new ESLintPlugin({
@@ -35,27 +47,36 @@ module.exports = {
     open: true,
     hot: true,
     client: {
-      logging: 'info'
+      logging: 'error',
+      overlay: false,
     },
     port: 9000
   },
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: babelOpts
+        },
+        {
+          loader: 'ts-loader',
+          options: {
+            // enable this option for hmr
+            // https://github.com/TypeStrong/ts-loader?tab=readme-ov-file#hot-module-replacement
+            transpileOnly: true
+          }
+        }],
+        exclude: /node_modules/,
+      },
+      {
         test: /\.(jsx|js)$/,
         include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
         use: [{
           loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults" 
-              }],
-              '@babel/preset-react'
-            ]
-            
-          }
+          options: babelOpts
         }]
       },
       {
@@ -75,7 +96,6 @@ module.exports = {
             'postcss-loader'
           ]
         }
-
     ]
   }
 }

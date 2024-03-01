5d47496be2192b2d046bbd0f3e220044d4a49116
Lucian <lucian.journey@gmail.com>
2024-03-01 13:27:45 +0800

    set devtool

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ const babelOpts = {
 
 module.exports = {
   mode: "development",
+  devtool: "cheap-module-source-map",
   entry: path.resolve(__dirname, 'src', 'index'),
   output: {
     path: path.resolve(__dirname, 'dist'),
